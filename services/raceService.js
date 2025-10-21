bd475fef35911c2b4ed90e9b309e5f705689c16d
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-22 04:51:37 +0900

    feat: 차 이동에 따른 실행 결과 출력기능 추가

--- services/raceService.js
+++ services/raceService.js
@@ -1,7 +1,17 @@
-import { moveCar } from "./carMovement";
+import { Console } from "@woowacourse/mission-utils";
+import { moveCar } from "./carMovement.js";
 
 export function playRace(cars, attempt) {
-  for (let i; i < attempt; i++) {
+  for (let i = 0; i < attempt; i++) {
     moveCar(cars);
+    printRaceResult(cars);
   }
+}
+
+export function printRaceResult(cars) {
+  Console.print("실행 결과");
+  cars.forEach((car) => {
+    Console.print(`${car.name} : ${"-".repeat(car.distance)}`);
+  });
+  Console.print("\n");
 }
--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import {
   validateAttempts,
   validateCarNames,
 } from "../utils/validation.js";
+import { playRace } from "../services/raceService.js";
 class App {
   async run() {
     try {
       const carArray = await Console.readLineAsync(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
       );
-      const cars = carArray.split(",").map((name) => name.trim());
-      validateCarNames(cars);
-      hasDuplicateNames(cars);
+      const cars = carArray
+        .split(",")
+        .map((name) => name.trim())
+        .map((name) => ({ name, distance: 0 }));
+      validateCarNames(cars.map((c) => c.name));
+      hasDuplicateNames(cars.map((c) => c.name));
 
       const num = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
       const attempt = Number(num);
       validateAttempts(attempt);
+
+      playRace(cars, attempt);
     } catch (e) {
       Console.print(`${e.message}`);
     }
