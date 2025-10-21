95912b7377777f63837c21b9ca043e63b178056b
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-22 02:39:04 +0900

    feat(validation): 자동차 입력 중복값 에러 처리 기능 추가

--- utils/validation.js
+++ utils/validation.js
@@ -3,7 +3,11 @@ export function validateCarNames(cars) {
     throw new Error("[ERROR] 자동차 이름은 1~5자여야 합니다.");
   }
 }
-
+export function hasDuplicateNames(cars) {
+  if (new Set(cars).size !== cars.length) {
+    throw new Error("[ERROR] 자동차 이름이 중복되어 있습니다.");
+  }
+}
 export function validateAttempts(attempt) {
   if (isNaN(attempt) || attempt < 1) {
     throw new Error("[ERROR] 시도 횟수는 1 이상의 숫자여야 합니다.");
