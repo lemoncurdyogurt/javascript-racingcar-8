aaaf5633b7827380c9291ab84208a6b0e9095f1b
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-22 02:20:44 +0900

    feat: 자동차 이름과 시도 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+import { validateAttempts, validateCarNames } from "../utils/validation.js";
 class App {
-  async run() {}
+  async run() {
+    try {
+      const carArray = await Console.readLineAsync(
+        "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+      );
+      const cars = carArray.split(",");
+      validateCarNames(cars);
+
+      const num = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+      const attempt = Number(num);
+      validateAttempts(attempt);
+    } catch (e) {
+      Console.print(`${e.message}`);
+    }
+  }
 }
 
 export default App;
---
+++ utils/validation.js
@@ -0,0 +1,11 @@
+export function validateCarNames(cars) {
+  if (!cars.every((name) => name && name.length <= 5)) {
+    throw new Error("[ERROR] 자동차 이름은 1~5자여야 합니다.");
+  }
+}
+
+export function validateAttempts(attempt) {
+  if (isNaN(attempt) || attempt < 1) {
+    throw new Error("[ERROR] 시도 횟수는 1 이상의 숫자여야 합니다.");
+  }
+}
