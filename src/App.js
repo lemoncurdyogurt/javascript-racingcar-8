88986971efce5cdb8948b3688f38be02da88d7e1
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-22 02:43:10 +0900

    fix(app): 자동차 이름 입력 시 공백 제거하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Console } from "@woowacourse/mission-utils";
-import { validateAttempts, validateCarNames } from "../utils/validation.js";
+import {
+  hasDuplicateNames,
+  validateAttempts,
+  validateCarNames,
+} from "../utils/validation.js";
 class App {
   async run() {
     try {
       const carArray = await Console.readLineAsync(
         "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
       );
-      const cars = carArray.split(",");
+      const cars = carArray.split(",").map((name) => name.trim());
       validateCarNames(cars);
+      hasDuplicateNames(cars);
 
       const num = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
       const attempt = Number(num);
